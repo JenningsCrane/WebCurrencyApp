6bee5846e33db7a340473262187c53c113248480
jenningc <dr.san4ess@mail.ru>
2024-03-27 08:23:40 +0300

    Ranis test security pls

--- src/main/java/ru/jenningc/webcurrency/dto/SignUpRequest.java
+++ src/main/java/ru/jenningc/webcurrency/dto/SignUpRequest.java
@@ -16,14 +16,19 @@ public class SignUpRequest {
 
     @Schema(description = "Имя пользователя", example = "Jon")
     @Size(min = 5, max = 50, message = "Имя пользователя должно содержать от 5 до 50 символов")
-    @NotBlank(message = "Имя пользователя не может быть пустыми")
+    @NotBlank(message = "Имя пользователя не может быть пустым")
     private String username;
 
     @Schema(description = "Адрес электронной почты", example = "jondoe@gmail.com")
     @Size(min = 5, max = 255, message = "Адрес электронной почты должен содержать от 5 до 255 символов")
-    @NotBlank(message = "Адрес электронной почты не может быть пустыми")
+    @NotBlank(message = "Адрес электронной почты не может быть пустым")
     @Email(message = "Email адрес должен быть в формате user@example.com")
     private String email;
+
+    @Schema(description = "Номер телефона", example = "+7(999)123-45-67")
+    @Size(min = 11, max = 17, message = "Номер телефона должен содержать минимум 11 символов")
+    @NotBlank(message = "Номер телефона не может быть пустым")
+    private String phoneNumber;
 
     @Schema(description = "Пароль", example = "my_1secret1_password")
     @Size(max = 255, message = "Длина пароля должна быть не более 255 символов")
